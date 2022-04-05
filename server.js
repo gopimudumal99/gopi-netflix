41cfb53fbd5bf01a30a2f3b052070b9752e87013
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-04-05 19:29:49 +0530

    add procfile

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require("express")
-const path = require('path')
-const port = process.env.PORT || 3000
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
-let initial_path = path.join(__dirname, "public")
+const port = process.env.PORT || 3000;
+
+let initial_path = path.join(__dirname, "public");
 
 let app = express();
-app.use(express.static(initial_path)) //express.static() method to make public folder as a static path
+app.use(cors());
+app.use(express.static(initial_path)); //express.static() method to make public folder as a static path
 
-app.get("/", (req, res) => { 
-    res.sendFile(path.join(initial_path, "Public/js/index.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(initial_path, "Public/js/index.html"));
+});
 
-app.get("/:id", (req, res) => { 
-    res.sendFile(path.join(initial_path,"about.html"))
-})
+app.get("/:id", (req, res) => {
+  res.sendFile(path.join(initial_path, "about.html"));
+});
 
-app.use((req, res) => { 
-    res.json("404")
-})
+app.use((req, res) => {
+  res.json("404");
+});
 
-app.listen(port, (req,res) => { 
-    console.log(`Listening on port ...${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`Listening on port ...${port}`);
+});
